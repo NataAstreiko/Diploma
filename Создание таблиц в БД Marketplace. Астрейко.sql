2690f1a921e526d51ac7d1f74c24fe4d924c9792
NataAstreiko <nata.astreiko@icloud.com>
2024-07-23 21:11:28 +0300

    Add files via upload

--- Создание таблиц в БД Marketplace. Астрейко.sql
+++ Создание таблиц в БД Marketplace. Астрейко.sql
@@ -1,17 +1,4 @@
-﻿
---1) Создала БД Marketplace. В ней создала 3 схемы:
---Production (таблицы Product, Category, Subcategory)
---SupplyChain (таблица Supplier)
---Sales (таблицы Revenue, Customer, PickupPoint, OrderDetail, DeliveryDetail)
-
---2) Написала запросы по созданию таблиц (но не ранала еще, жду ОС от тебя:)
-
---3) Обновила схему БД актуальными названиями и типами данных
-
---4) Подгрузила все в гит в ветку Diploma
-
-
-CREATE TABLE Sales.OrderDetail
+﻿CREATE TABLE Sales.OrderDetail
 (
 OrderID INT PRIMARY KEY,
 OrderDate DATETIME NOT NULL,
@@ -25,34 +12,34 @@ DeliveryDate DATETIME NOT NULL,
 DeliveredItems INT NOT NULL
 );
 
-CREATE TABLE Production.Category
+CREATE TABLE Sales.Category
 (
 CategoryID INT PRIMARY KEY,
 CategoryName VARCHAR (50) NOT NULL
 );
 
-CREATE TABLE Production.Subcategory
+CREATE TABLE Sales.Subcategory
 (
 SubcategoryID INT PRIMARY KEY,
 SubcategoryName VARCHAR (100) NOT NULL,
-CategoryID INT FOREIGN KEY (CategoryID) REFERENCES Production.Category (CategoryID) NOT NULL
+CategoryID INT FOREIGN KEY (CategoryID) REFERENCES Sales.Category (CategoryID) NOT NULL
 );
 
-CREATE TABLE SupplyChain.Supplier
+CREATE TABLE Sales.Supplier
 (
 SupplierID INT PRIMARY KEY,
 CompanyName VARCHAR (50) UNIQUE,
 BrandName VARCHAR (50) DEFAULT 'n.d'
 );
 
-CREATE TABLE Production.Product
+CREATE TABLE Sales.Product
 (
 ProductID INT PRIMARY KEY,
 ProductName VARCHAR (100) NOT NULL,
 Price DECIMAL (18,2) DEFAULT 1,
 Promo BIT NOT NULL,
-SubcategoryID INT FOREIGN KEY (SubcategoryID) REFERENCES Production.Subcategory (SubCategoryID) NOT NULL,
-SupplierID INT FOREIGN KEY (SupplierID) REFERENCES SupplyChain.Supplier (SupplierID) NOT NULL
+SubcategoryID INT FOREIGN KEY (SubcategoryID) REFERENCES Sales.Subcategory (SubCategoryID) NOT NULL,
+SupplierID INT FOREIGN KEY (SupplierID) REFERENCES Sales.Supplier (SupplierID) NOT NULL
 );
 
 CREATE TABLE Sales.PickupPoint
@@ -68,9 +55,9 @@ CREATE TABLE Sales.Customer
 CustomerID INT PRIMARY KEY,
 CustomerName VARCHAR (25) NOT NULL,
 CustomerSurname VARCHAR (25) NOT NULL,
-FullName AS CONCAT(CustomerName,' ',CustomerSurname), --нужно ли тут указывать тип данных?
+FullName AS CONCAT(CustomerName,' ',CustomerSurname),
 BirthDate DATETIME NOT NULL,
-Age AS GETDATE()-BirthDate, -- вот тут не уверена, как будто так нельзя :)) и нужно ли тут указывать тип данных?
+Age AS  DATEDIFF (year, BirthDate, GETDATE()),
 Gender VARCHAR(2),
 PhoneNumber TINYINT,
 City VARCHAR (25),
@@ -78,11 +65,13 @@ Activity BIT NOT NULL,
 PickupPointID INT FOREIGN KEY (PickupPointID) REFERENCES Sales.PickupPoint (PickupPointID)
 );
 
+DROP TABLE Sales.Customer
+DROP TABLE Sales.Revenue
 
 CREATE TABLE Sales.Revenue
 (
 OrderID INT FOREIGN KEY (OrderID) REFERENCES Sales.OrderDetail (OrderID) NOT NULL,
-ProductID INT FOREIGN KEY (ProductID) REFERENCES Production.Product (ProductID) NOT NULL,
+ProductID INT FOREIGN KEY (ProductID) REFERENCES Sales.Product (ProductID) NOT NULL,
 DeliveryID INT FOREIGN KEY (DeliveryID) REFERENCES Sales.DeliveryDetail (DeliveryID) NOT NULL,
 CustomerID INT FOREIGN KEY (CustomerID) REFERENCES Sales.Customer (CustomerID) NOT NULL,
 Revenue DECIMAL (18,2),
